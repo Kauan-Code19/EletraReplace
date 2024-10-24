623b486800ff5c9e8c722a001b1fdaffcaf08de9
Kauan-Code19 <pkauprofissional@gmail.com>
2024-10-24 02:04:50 -0300

    ajuste em todas as chamadas de caminho

--- js/autenticacao.js
+++ js/autenticacao.js
@@ -5,7 +5,7 @@ function estaAutenticado() {
         return
     }
 
-    window.location.href = '../index.html'
+    window.location.href = 'index.html'
 }
 
 window.onload = estaAutenticado
--- js/rodape.js
+++ js/rodape.js
@@ -1,5 +1,5 @@
 function carregarRodape() {
-    fetch('../html/rodape.html')
+    fetch('html/rodape.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer-container').innerHTML = data;
